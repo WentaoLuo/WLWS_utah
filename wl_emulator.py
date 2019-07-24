eafbc7455a7b9cb3387b045a50c6ef3e9f2e48a6
WentaoLuo <wentao.luo@yale.edu>
2019-07-24 13:25:44 +0900

    finish the mock and noise almost done

--- wl_emulator.py
+++ wl_emulator.py
@@ -33,8 +33,9 @@ Gg    =6.67408e-11
 ckm   =3.240779e-17            
 ckg   =5.027854e-31           
 fac   =(vc*vc*ckg)/(4.*pi*Gg*ckm) 
+resp,errorp = np.loadtxt('parents_dist.dat',unpack=True) 
 
-
+Nboot = 500
 #--Basic cosmology calculations--------------------
 def efunclcdm(x):
    res = 1.0/np.sqrt(Om0*(1.0+x)**3+Ok0*(1.0+x)**2+Ol0*(1.0+x)**(3*(1.0+w)))
@@ -104,22 +105,23 @@ def nfwesd(theta,z,Rp):
   return esd
 
 def Rpbins(theta,Nbin,z):
-  Rmax = 2.0
-  Rmin = 0.1
-  dl   = Da(z)
-  zs   = 0.45
-  ds   = Da(zs)
-  Sig  =fac*ds/(dl*(ds-dl))/(1.0+z)/(1.0+z)
-  rbin = np.zeros(Nbin+1)
-  r    = np.zeros(Nbin)
-  xtmp = (np.log10(Rmax)-np.log10(Rmin))/Nbin
-  area = np.zeros(Nbin)
-  ngals= np.zeros(Nbin)
-  esd  = np.zeros(Nbin)
+  Rmax  = 2.0
+  Rmin  = 0.02
+  dl    = Da(z)
+  zs    = 0.45
+  ds    = Da(zs)
+  Sig   =fac*ds/(dl*(ds-dl))/(1.0+z)/(1.0+z)
+  rbin  = np.zeros(Nbin+1)
+  r     = np.zeros(Nbin)
+  xtmp  = (np.log10(Rmax)-np.log10(Rmin))/Nbin
+  area  = np.zeros(Nbin)
+  ngals = np.zeros(Nbin)
+  esd   = np.zeros(Nbin)
   esdnfw= np.zeros(Nbin)
-  err  = np.zeros(Nbin)
-  the1 = np.zeros(Nbin)
-  the2 = np.zeros(Nbin)
+  #err   = np.zeros(Nbin)
+  #wts   = np.zeros(Nbin)
+  the1  = np.zeros(Nbin)
+  the2  = np.zeros(Nbin)
   for i in range(Nbin):
     ytmp1 = np.log10(Rmin)+float(i)*xtmp
     ytmp2 = np.log10(Rmin)+float(i+1)*xtmp
@@ -127,58 +129,96 @@ def Rpbins(theta,Nbin,z):
     rbin[i+1] = 10.0**ytmp2
     the1  = np.arctan(rbin[i]/dl)*180.0*60.0/pi
     the2  = np.arctan(rbin[i+1]/dl)*180.0*60.0/pi
-    area[i]= pi*(the2**2-the1**2)
+    area[i]= 20.0*pi*(the2**2-the1**2)
     r[i] =(rbin[i])*1./2.+(rbin[i+1])*1./2.
     ngals[i]= np.random.poisson(lam=area[i]) 
     if ngals[i]>0:
       esd[i]= nfwesd(theta,z,r[i])+Sig*np.random.normal(loc=0.0,scale=0.4)/np.sqrt(float(ngals[i]))
-      esdnfw[i]= nfwesd(theta,z,r[i])
-      err[i]= Sig*np.random.normal(loc=0.0,scale=0.4)/np.sqrt(float(ngals[i]))
+      esdnfw[i]= nfwesd(theta,z,r[i])/Sig
+      #err[i]= Sig*np.random.normal(loc=0.0,scale=0.4)/np.sqrt(float(ngals[i]))
+      
+  #return {'radius':r,'NUM':ngals,'ESD':esd,'NFW':esdnfw,'error':err}
   return {'radius':r,'NUM':ngals,'ESD':esd,'NFW':esdnfw}
+
 #----------------------------------------------------------------
 def main():
-   Nbin = 10
-   #Nlens= 10000
-   fmock= '../mock/mock_f0.10_s01.dat' 
-
    Nboot= 500
-   esdall= np.zeros((Nlens,Nbin))
-   esdmdl= np.zeros((Nlens,Nbin))
-   esdmean= np.zeros(Nbin)
-   esdmodel= np.zeros(Nbin)
-   esderr = np.zeros(Nbin)
-   Ngax   = np.zeros((Nlens,Nbin))
+   import sys
+   fmock= '../mock/mock_f0.10_s02.dat' 
+   Mr,zl,zmax,Vmax,lgMh,R,icen = np.loadtxt(fmock,unpack=True,comments='#')
+   icena= icen ==1
+   icenb= Mr<= float(sys.argv[1])
+   icenc= Mr>= float(sys.argv[2])
+   idcen= icena&icenb&icenc
+
+   idsat= icen ==0
+   Mrcen= Mr[idcen]
+   Mrsat= Mr[idsat]
+   zlcen= zl[idcen]
+   zlsat= zl[idsat]
+   Vcen = Vmax[idcen]
+   Vsat = Vmax[idsat]
+   Mhcen= lgMh[idcen]
+   Nlens= len(Mhcen)
+#------------------------------------------------------------------
+   zs    = 0.45
+   ds    = Da(zs)
+   sige  = 0.4
+   Nbin  = 10
+   shear = np.zeros((Nlens,Nbin))
+   Ngax  = np.zeros((Nlens,Nbin))
+
+   sumshrI = np.zeros(Nbin)
+   sumwhtI = np.zeros(Nbin)
+   sumshrII= np.zeros(Nbin)
+   sumwhtII= np.zeros(Nbin)
    for i in range(Nlens):
-     Mh   = 14.0
-     c    = 5.0
-     zl   = 0.1
-     info = Rpbins([Mh,c],Nbin,zl)
-     rr   = info['radius']
-     esdall[i,:]  = info['ESD']
-     esdmdl[i,:]  = info['NFW']
+     Mh   = Mhcen[i]
+     c    = 4.67*(10.0**(Mh-14)*h)**(-0.11) # Neto et al 2007 
+     info = Rpbins([Mh,c],Nbin,zl[i])
+     Rp   = info['radius']
+     shear[i,:] = info['NFW']
      Ngax[i,:]  = info['NUM']
-     #err  = info['ERR']
-   for j in range(Nbin):
-     temp  = np.zeros((Nbin,Nboot))
-     for iboot in range(Nboot):
-	idx = np.random.randint(low=0,high=Nlens,size=Nlens)
-#	print idx
-        temp[j,iboot]= np.nanmean(esdall[idx,j])
-     esderr[j]= np.nanstd(temp[j,:])
-     esdmean[j]= np.nanmean(temp[j,:])
-     esdmodel[j]= np.nanmean(esdmdl[:,j])
-   #print esdmean,esderr
+     dl   = Da(zl[i])*(1.0+zl[i])
+     Sig  =fac*ds/(dl*(ds-dl))/(1.0+zl[i])/(1.0+zl[i])
+     for j in range(Nbin):
+          ngal  = int(np.around(Ngax[i,j]))
+	  #print ngal
+	  isx   = np.random.randint(low=0,high=len(errorp),size=ngal)
+	  #print j,shear[i,j]
+          gmt   = Sig*np.random.normal(loc=shear[i,j],scale=0.4,size=ngal)
+	  wht   = 1.0/(sige*sige+errorp[isx]**2)/Sig/Sig
+	  #print gmt.sum(),wht.sum()
+          sumshrI[j] = sumshrI[j]+(gmt*wht).sum()
+          sumwhtI[j] = sumwhtI[j]+wht.sum()
+          sumshrII[j]= sumshrII[j]+(gmt*wht).sum()/Vcen[i]
+          sumwhtII[j]= sumwhtII[j]+wht.sum()/Vcen[i]
+
+   gammaI  = sumshrI/sumwhtI
+   gammaII = sumshrII/sumwhtII
+   Mhm     = np.mean(Mhcen)
+   zm      = np.mean(zl)
+   con     = 4.67*(10.0**(Mhm-14)*h)**(-0.11) # Neto et al 2007 
+   esdnfw  = np.zeros(Nbin)
+   for i in range(Nbin):
+     esdnfw[i]  = nfwesd([Mhm,con],zm,Rp[i])
+     print Rp[i], gammaI[i]/h,gammaII[i]/h,esdnfw[i]/h
+     #print gammaII
+   """ 
    plt.figure(figsize=[9,6])
-   plt.errorbar(rr,esdmean,yerr=esderr,fmt='k.',ms=20,elinewidth=3,label='Mock')
-   plt.plot(rr,esdmodel,'k-',linewidth=3,label='NFW')
+   #plt.errorbar(Rp,gammaI,yerr=esderr,fmt='k.',\
+   #             ms=20,elinewidth=3,label='weight I')
+   plt.plot(Rp,gammaII/h,'k-',linewidth=3,label='weight volum limited')
+   plt.plot(Rp,gammaI/h,'k--',linewidth=3,label='weight traditional')
+   plt.plot(Rp,esdnfw/h,'k:',linewidth=3,label='<Mh> NFW model')
    plt.xlabel('R ($h^{-1}kpc$)',fontsize=20)
    plt.ylabel('ESD ($M_{\odot}/pc^2)$',fontsize=20)
    plt.legend()
    plt.xscale('log')
    plt.yscale('log',nonposy='clip')
-   plt.xlim(0.1,1.5)
-   plt.ylim(5,200)
+   plt.xlim(0.02,2.0)
+   plt.ylim(0.01,1000)
    plt.show()
-
+   """ 
 if __name__=='__main__':
    main()
